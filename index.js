18f478d5448ce5a6b3fb964afecdb8c003870ed8
Brook Jordan <brkjrdn@gmail.com>
2017-03-01 23:22:50 +0800

    staticise

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const path    = require('path');
+const app     = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
-
 app.get('/', function(request, response) {
-  response.render('index.html');
+  response.send('<h1>Hi!</h1>');
 });
 
 app.listen(app.get('port'), function() {
